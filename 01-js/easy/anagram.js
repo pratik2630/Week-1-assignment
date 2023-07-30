1b75f00b6ee7d07a15a499ed593a2faf8210c066
pratik2630 <pratikkorgaonkar26@gmail.com>
2023-07-31 00:03:18 +0530

    Completed anagram code!!

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -7,8 +7,32 @@
   - `npm run test-anagram`
 */
 
+function sortWord(AnagramWord) {
+   let anagramArray = AnagramWord.split("")
+   anagramArray.sort()
+   wordJoin = anagramArray.join("")
+   //console.log(wordJoin)
+   return(wordJoin)
+}
+
 function isAnagram(str1, str2) {
+  if (str1==str2) {
+    return("Its anagram")
+  } else {
+
+    if (sortWord(str1)==sortWord(str2)) {
+      return("Its anagram!")
+    } else {
+      return("It's not anagramS")
+    }
+    
+  }
 
 }
 
+str1 = "listen"
+str2 = "Silent"
+answer = isAnagram(str1.toLowerCase(),str2.toLowerCase())
+console.log(answer)
+sortWord("heloo")
 module.exports = isAnagram;
